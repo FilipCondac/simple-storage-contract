deff85dc345a96b9cfc14b911b84e12c8686945d
Filip <filipcondac@gmail.com>
2025-06-18 17:17:27 +0100

    Add SimpleStorage.sol

---
+++ SimpleStorage.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract SimpleStorage {
+
+    uint256 public myFavoriteNumber;
+
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople; 
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns (uint256){
+        return myFavoriteNumber;
+    }
+
+    function addFriend(uint256 _favoriteNumber, string memory name) public {
+        listOfPeople.push(Person({favoriteNumber:_favoriteNumber, name: name}));
+        nameToFavoriteNumber[name] = _favoriteNumber;
+    }
+
+}
+
