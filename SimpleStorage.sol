2264d2ded66651139d2da0ad53447599881426a9
Filip <filipcondac@gmail.com>
2025-06-30 13:36:05 +0100

    Overriding functions and getters

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -14,6 +14,35 @@ contract SimpleStorage {
 
     mapping(string => uint256) public nameToFavoriteNumber;
 
+    function store(uint256 _favoriteNumber) public virtual{
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns (uint256){
+        return myFavoriteNumber;
+    }
+
+    function addFriend(uint256 _favoriteNumber, string memory name) public {
+        listOfPeople.push(Person({favoriteNumber:_favoriteNumber, name: name}));
+        nameToFavoriteNumber[name] = _favoriteNumber;
+    }
+
+}
+
+
+contract SimpleStorage2 {
+
+    uint256 public myFavoriteNumber;
+
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople; 
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
     function store(uint256 _favoriteNumber) public {
         myFavoriteNumber = _favoriteNumber;
     }
@@ -28,4 +57,5 @@ contract SimpleStorage {
     }
 
 }
+
 
--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,6 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract StorageFactory{
-    
+import { SimpleStorage } from "./SimpleStorage.sol";
+
+contract StorageFactory {
+    SimpleStorage[] public listOfSimpleStorageContracts;
+
+
+    function createSimplestorageContract() public {
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        SimpleStorage mySimplestorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        mySimplestorage.store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
+    }
 }
